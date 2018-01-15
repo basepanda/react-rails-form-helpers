cdfe45938f0894f7bfc757f8d3aa22a8655bfefc
BasePanda <alexkonanmcdaid@gmail.com>
2018-01-15 10:03:23 +0000

    react-md

--- src/tags.jsx
+++ src/tags.jsx
@@ -1,5 +1,6 @@
 import { whitelistProps } from "./utils"
 import React from 'react'
+import { TextField, Button } from 'react-md';
 
 export const CheckBoxTag = ({
   checkedValue = 1,
@@ -14,7 +15,7 @@ export const CheckBoxTag = ({
   )
 }
 
-export const ColorFieldTag = props => <input type="color" {...props} />
+export const ColorFieldTag = props => <TextField type="color" {...props} />
 
 export const DateFieldTag = props => <input type="date" {...props} />
 
@@ -24,7 +25,7 @@ export const DatetimeLocalFieldTag = props => (
   <input type="datetime-local" {...props} />
 )
 
-export const EmailFieldTag = props => <input type="email" {...props} />
+export const EmailFieldTag = props => <TextField type="email" {...props} />
 
 export const HiddenFieldTag = props => (
   <input type="hidden" readOnly {...props} />
@@ -34,9 +35,9 @@ export const LabelTag = props => <label {...whitelistProps(props)} />
 
 export const MonthFieldTag = props => <input type="month" {...props} />
 
-export const NumberFieldTag = props => <input type="number" {...props} />
+export const NumberFieldTag = props => <TextField type="number" {...props} />
 
-export const PasswordFieldTag = props => <input type="password" {...props} />
+export const PasswordFieldTag = props => <TextField type="password" {...props} />
 
 export const RadioTag = props => <input type="radio" {...props} />
 
@@ -46,16 +47,16 @@ export const SearchFieldTag = props => <input type="search" {...props} />
 
 export const SelectTag = props => <select {...whitelistProps(props)} />
 
-export const SubmitTag = props => <input type="submit" {...props} />
+export const SubmitTag = props => <Button raised primary type="submit" {...props} />
 
-export const TelephoneFieldTag = props => <input type="tel" {...props} />
+export const TelephoneFieldTag = props => <TextField type="tel" {...props} />
 
 export const TextAreaTag = props => <textarea {...whitelistProps(props)} />
 
-export const TextFieldTag = props => <input type="text" {...props} />
+export const TextFieldTag = props => <TextField type="text" {...props} />
 
 export const TimeFieldTag = props => <input type="time" {...props} />
 
-export const UrlFieldTag = props => <input type="url" {...props} />
+export const UrlFieldTag = props => <TextField type="url" {...props} />
 
 export const WeekFieldTag = props => <input type="week" {...props} />
